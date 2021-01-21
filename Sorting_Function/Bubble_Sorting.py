03d56972032ca385a2f8857a511f89e22b6a8d07
Rishav Kumar Mishra <rishav.mishra.rkm@gmail.com>
2021-01-21 20:50:29 +0530

    Update Bubble_Sorting.py

--- Sorting_Function/Bubble_Sorting.py
+++ Sorting_Function/Bubble_Sorting.py
@@ -1,4 +1,4 @@
-def bubblesort(A):
+def bubble_sort(A):
     n = len(A)
     for passes in range(n-1,0,-1):
         for i in range(passes):
@@ -9,6 +9,6 @@ def bubblesort(A):
 
 
 A = [3, 5, 8, 9, 6, 2]
-print('Original Array:',A)
-bubblesort(A)
-print('Sorted Array:',A)
+print('Original Array : ',A)
+bubble_sort(A)
+print('Sorted Array : ',A)
